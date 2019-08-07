16f3ee4c1d1120da9c80102f846245294da3438d
Ping Chen <remotenonsense@gmail.com>
2019-08-07 20:36:30 +0900

    Add render to reuse MarkdownIt instance

--- src/MarkdownIt/Renderer/Halogen.purs
+++ src/MarkdownIt/Renderer/Halogen.purs
@@ -1,5 +1,6 @@
 module MarkdownIt.Renderer.Halogen
   ( renderString
+  , render
   ) where
 
 import Prelude
@@ -9,6 +10,13 @@ import Halogen.HTML as HH
 import Html.Parser.Halogen as PH
 import MarkdownIt as MD
 
+-- | Render markdown string into Halogen HTML.
 renderString :: forall p i. String -> HH.HTML p i
 renderString input =
   PH.render $ unsafePerformEffect $ MD.renderString input
+
+-- | Use an existing MarkdownIt instance to render markdown string into Halogen
+-- | HTML.
+render :: forall p i. MD.MarkdownIt -> String -> HH.HTML p i
+render md input =
+  PH.render $ unsafePerformEffect $ MD.render input md
