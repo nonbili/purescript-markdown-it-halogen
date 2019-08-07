6d3740d451f84714944f7c03e3e5336e0e64d960
Ping Chen <remotenonsense@gmail.com>
2019-08-07 18:22:04 +0900

    Add example

---
+++ example/src/App.purs
@@ -0,0 +1,73 @@
+module App where
+
+import Prelude
+
+import Data.Const (Const)
+import Data.Maybe (Maybe(..))
+import Halogen as H
+import Halogen.HTML as HH
+import Halogen.HTML.Events as HE
+import Halogen.HTML.Properties as HP
+import MarkdownIt.Renderer.Halogen as MRH
+
+type Query = Const Void
+
+data Action = OnValueChange String
+
+type State = { value:: String }
+
+
+initValue :: String
+initValue = """
+# Markdown in Halogen
+
+Powered by [purescript-markdown-it](https://github.com/nonbili/purescript-markdown-it) and [purescript-html-parser-halogen](https://github.com/rnons/purescript-html-parser-halogen).
+"""
+
+initialState :: State
+initialState = { value: initValue }
+
+class_ :: forall r i. String -> HP.IProp ("class" :: String | r) i
+class_ = HP.class_ <<< HH.ClassName
+
+style :: forall r i. String -> HP.IProp ("style" :: String | r) i
+style = HP.attr (HH.AttrName "style")
+
+render :: forall m. State -> H.ComponentHTML Action () m
+render state =
+  HH.div [ class_ "grid" ]
+  [ HH.h2 [ class_ "header" ]
+    [ HH.text "purescript-markdown-it-halogen example" ]
+  , HH.div [ class_ "col col-edit" ]
+    [ HH.h4_ [ HH.text "EDIT" ]
+    , HH.textarea
+      [ class_ "edit"
+      , HP.value state.value
+      , HE.onValueInput $ Just <<< OnValueChange
+      ]
+    ]
+  , HH.div [ class_ "col col-preview" ]
+    [ HH.h4_ [ HH.text "PREVIEW" ]
+    , HH.div [ class_ "preview" ]
+      [ MRH.renderString state.value ]
+    ]
+  , HH.div [ class_ "footer" ]
+    [ MRH.renderString "[source code](https://github.com/nonbili/purescript-markdown-it/halogen/tree/master/example)"
+    ]
+  ]
+  where
+  repoUrl = "https://github.com/rnons/purescript-html-parser-halogen"
+  demoSourceUrl = repoUrl <> "/tree/master/example"
+
+app :: forall m. H.Component HH.HTML Query Unit Void m
+app = H.mkComponent
+  { initialState: const initialState
+  , render
+  , eval: H.mkEval $ H.defaultEval
+      { handleAction = handleAction }
+  }
+
+handleAction :: forall m. Action -> H.HalogenM State Action () Void m Unit
+handleAction = case _ of
+  OnValueChange value -> do
+    H.modify_ $ _ { value = value }
---
+++ example/webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  context: __dirname,
+  entry: "./src/index.js",
+  output: {
+    path: __dirname + "/../docs",
+    filename: "index.js"
+  },
+  resolve: {
+    modules: ["node_modules", "output"],
+    extensions: [".js"]
+  },
+  module: {
+    rules: []
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+      filename: "index.html",
+      minify: {
+        collapseWhitespace: true,
+        minifyCSS: true
+      }
+    })
+  ]
+};
